f87cdc20031c4cfae89b711c4879c385c92cd39f
alex.drouillard <alex.drouillard.9@hotmail.com>
2023-11-17 12:24:29 -0500

    final commit

--- src/pages/RecommendationsPage/RecommendationsPage.js
+++ src/pages/RecommendationsPage/RecommendationsPage.js
@@ -7,13 +7,26 @@ export function RecommendationsPage({ titleList }) {
     const [services, setServices] = useState([]);
 
     useEffect(() => {
+        console.log(titleList);
         const fetchServices = async () => {
-            const { data } = await axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/services`);
+            const titleCounts = {};
+            for(let i = 0; i < titleList.length; i++) {
+                const service = titleList[i].service_id;
+                if (titleCounts[service] === undefined) {
+                    titleCounts[service] = 0;
+                }
+                titleCounts[service] += 1;
+            }
+            const requests = Object.keys(titleCounts).map(service => {
+                return axios.get(`${process.env.REACT_APP_BACKEND_URL}/api/services/${service}`);
+            })
+            const responses = await Promise.all(requests);
+            const data = responses.map(response => ({...response.data, count: titleCounts[response.data.id]}));
             setServices(data);
         };
 
-        fetchServices();
-    }, []);
+        titleList.length && fetchServices();
+    }, [titleList]);
 
     return <main className="recommendations">
         <div className="recommendations__heading">
@@ -22,7 +35,7 @@ export function RecommendationsPage({ titleList }) {
         </div>
             <article>
             <div className="recommendations__platform">
-                {services.length && (
+                {!!services.length &&(
                 services.map((service, index) =>
                 <div key={service.id}>
                     <div className="recommendations__platform--top">
@@ -30,15 +43,18 @@ export function RecommendationsPage({ titleList }) {
                         <img src={`${process.env.REACT_APP_BACKEND_URL}${service.logo}`} alt={service.name} className="recommendations__platform--img"/>
                     </div>
                     <div className="recommendations__platform--text">
-                        <p>{service.name} has X of the titles you selected.</p>
-                        <p>Base pricing for ads plan is {service.price}.</p>
+                        <p><span className="recommendations__platform--name">{service.name}</span> has <span className="recommendations__platform--name">{service.count}</span> of the titles you selected.</p>
+                        <p>Base pricing for ads plan is $5.99.</p>
                     </div>
                     <div className="recommendations__platform--bottom">
-                    <Link to=""><button className="recommendations__platform--button">Sign up for {service.name}</button></Link>
+                    <button className="recommendations__platform--button">Sign up now</button>
                     </div>
                 </div>
                     ))}
                 </div>
             </article>
+        <div className="recommendations__footer">
+        <Link to="/"><button className="recommendations__footer--button">Refine your results</button></Link>
+        </div>
     </main>
 }
