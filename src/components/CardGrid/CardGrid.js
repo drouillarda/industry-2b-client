d561df36f71dc9df7f6b93b8753df0585d3002d7
Alex Howley <howley.alex@gmail.com>
2023-11-16 23:37:04 -0600

    Create and style grid component

---
+++ src/components/CardGrid/CardGrid.js
@@ -0,0 +1,17 @@
+import "./CardGrid.scss";
+import { Card } from "../Card/Card";
+
+export const CardGrid = ({ heading, records, titleField, imageField }) => (
+  <section className="grid">
+    <h2 className="grid__heading">{heading}</h2>
+    <div className="grid__grid">
+      {records.map(record =>
+        <Card
+          key={record.id}
+          title={record[titleField]}
+          backgroundUrl={imageField}
+          handleAddButton={() => {}}
+        />)}
+    </div>
+  </section>
+);
