52029d6c6d03f4de6da3a1d69819d9b042b6bfdb
Alex Howley <howley.alex@gmail.com>
2023-11-16 22:44:06 -0600

    Finish styling card component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -6,7 +6,8 @@ export function HomePage() {
     return <main className="home">
         <Card
             title="Test Card"
-            description="This is kind of an awkward, long test description for this card component."
+            bigText={true}
+            // description="This is kind of an awkward, long test description for this card component."
             backgroundUrl="https://www.thesoapopera.com/cdn/shop/products/The-Soap-Opera-Rubber-Ducks-Colors-Yellow_02cd30bb-0dbd-4fdc-8d7c-6ee1be5a04e8_590x.jpg?v=1613344659"
         />
     </main>
