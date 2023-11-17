68a1ad0e124aa30801e278ab53e07fd8780479cc
Alex Howley <howley.alex@gmail.com>
2023-11-16 20:51:17 -0600

    AMake hcard background image optional

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,8 +1,8 @@
 import "./Card.scss";
 
 export const Card = ({ title, backgroundUrl, description,  }) => (
-  <article className="card" style={{ "--bg-image": `url("${backgroundUrl}")` }}>
-    <h2 className="card__subheading">{title}</h2>
+  <article className="card" style={backgroundUrl && { "--bg-image": `url("${backgroundUrl}")` }}>
+    <h3 className="card__subheading">{title}</h3>
     <p className="card__description">{description}</p>
   </article>
 );
