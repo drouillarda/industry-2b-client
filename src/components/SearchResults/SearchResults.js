27555a3ef687c64690fe25ec1c18d9a577253394
alex.drouillard <alex.drouillard.9@hotmail.com>
2023-11-17 00:46:18 -0500

    minor changes

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -4,7 +4,6 @@ export const SearchResults = () => {
     return (
         <section className='searchResults'>
             <h1>Search Results</h1>
-            <h3>Add title to your list</h3>
         </section>
     );
 };
