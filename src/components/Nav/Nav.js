7258b204017729237f0f9d991771b1d2b171aab0
alex.drouillard <alex.drouillard.9@hotmail.com>
2023-11-16 23:46:22 -0500

    Initial nav commit, items/assets on page

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,9 +1,17 @@
 import "./Nav.scss"
+import popcorn from './../../assets/images/emojione-monotone-popcorn.svg';
 import { Link } from "react-router-dom";
 
 export const Nav = () => {
     return (
-        <header className="navbar">
+        <header className="nav">
+            <div className="nav__start">
+                <Link to="/" className="nav__logo"><h3>Website Name</h3></Link>
+            </div>
+            <div className="nav__end">
+                <Link to="/upload"><img src={popcorn} className="nav__hidden" alt="popcorn"/></Link>
+                <p>My List</p>
+            </div>
         </header>
     );
 };
