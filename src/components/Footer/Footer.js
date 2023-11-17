3ef62662f5b5a510bd78dfde5ea4444195727861
Alex Howley <howley.alex@gmail.com>
2023-11-16 21:30:59 -0600

    Add known colors and font defs, with basic global styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,8 +2,6 @@ import "./Footer.scss"
 
 export const Footer = () => {
     return (
-        <footer>
-                <p className="footer__text">© InStock Inc. All Rights Reserved.</p>
-        </footer>
-    )
-}
+        <></>
+    );
+};
