d905c41d784580b49a6ff10a4cabda8a96460911
Alex Howley <howley.alex@gmail.com>
2023-11-17 08:12:14 -0600

    Add animate-in to card grid

--- src/components/CardGrid/CardGrid.js
+++ src/components/CardGrid/CardGrid.js
@@ -1,18 +1,33 @@
 import "./CardGrid.scss";
 import { Card } from "../Card/Card";
+import { useEffect, useState } from "react";
 
-export const CardGrid = ({ heading, records, titleField, imageField, handleAddButton, titleList }) => (
-  <section className="grid">
-    <h2 className="grid__heading">{heading}</h2>
-    <div className={`grid__grid ${records.length === 1 ? "grid__grid--single" : ""}`}>
-      {records.map(record =>
-        <Card
+export const CardGrid = ({ heading, records, titleField, imageField, handleAddButton, titleList }) => {
+  const [quantityToShow, setQuantityToShow] = useState(1);
+
+  useEffect(() => {
+    const addToQuantityShowing = (duration) => {
+      if (quantityToShow < records.length) {
+        setTimeout(() => {
+          setQuantityToShow(prev => prev + 1);
+          addToQuantityShowing(duration * 0.95);
+        }, Math.max(duration, 50));
+      }
+    }
+    addToQuantityShowing(100);
+  }, [records]);
+
+  return (
+    <section className="grid">
+      <h2 className="grid__heading">{heading}</h2>
+      <div className={`grid__grid ${records.length === 1 ? "grid__grid--single" : ""}`}>
+        {records.map(record => <Card
           key={record.id}
           title={record[titleField]}
           backgroundUrl={record[imageField]}
           handleAddButton={() => handleAddButton(record)}
-          inList={titleList.map(entry => entry.id).includes(record.id)}
-        />)}
-    </div>
-  </section>
-);
+          inList={titleList.map(entry => entry.id).includes(record.id)} />).slice(0, quantityToShow)}
+      </div>
+    </section>
+  );
+};
