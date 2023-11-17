fe19b4fa7fbc5a8d86983e68b5b04bf06d328a35
Alex Howley <howley.alex@gmail.com>
2023-11-17 08:26:09 -0600

    Fix unselect animation bug

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,16 +1,27 @@
 import "./Card.scss";
 import addIcon from "../../assets/images/add-to-cart.svg";
+import { useEffect, useState } from "react";
 
-export const Card = ({ title, backgroundUrl, description, handleAddButton, bigText, inList }) => (
-  <article
-    className={`card ${bigText ? "card--big" : ""} ${inList ? "card--in-list" : ""}`}
-    style={backgroundUrl && { "--bg-image": `url("${backgroundUrl}")` }}
-    onClick={handleAddButton}
-  >
-    <button type="button" className="card__add-button">
-      <img src={addIcon} alt="A plus sign, representing adding to your list." />
-    </button>
-    <h3 className="card__subheading">{title}</h3>
-    {description && <p className="card__description">{description}</p>}
-  </article>
-);
+export const Card = ({ title, backgroundUrl, description, handleAddButton, bigText, inList }) => {
+  const [noAnimate, setNoAnimate] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => setNoAnimate(true), 500);
+  }, []);
+  
+  return (
+    <article
+      className={
+        `card ${bigText ? "card--big" : ""} ${inList ? "card--in-list" : ""} ${noAnimate ? "card--no-animate" : ""}`
+      }
+      style={backgroundUrl && { "--bg-image": `url("${backgroundUrl}")` }}
+      onClick={handleAddButton}
+    >
+      <button type="button" className="card__add-button">
+        <img src={addIcon} alt="A plus sign, representing adding to your list." />
+      </button>
+      <h3 className="card__subheading">{title}</h3>
+      {description && <p className="card__description">{description}</p>}
+    </article>
+  );
+};
