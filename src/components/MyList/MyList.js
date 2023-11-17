e88f942e43c39616dd90f2ca92000157bbbfbf79
claratam <claramtam@gmail.com>
2023-11-17 12:50:38 -0600

    feat: add desktop styling for listpage

--- src/components/MyList/MyList.js
+++ src/components/MyList/MyList.js
@@ -1,6 +1,5 @@
 import './MyList.scss';
 import { CardGrid } from '../CardGrid/CardGrid';
-import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 export const MyList = ({ titleList, handleAdd }) => {
